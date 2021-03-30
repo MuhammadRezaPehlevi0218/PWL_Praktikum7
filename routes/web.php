f0f3d3ed66cc123970952cbd2ff516dcef75069d
MuhammadRezaPahlevi0218 <reyzae99@gmail.com>
2021-03-30 15:20:55 +0700

    membuat reoute pada blade.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\MahasiswaController;
+use App\Models\Mahasiswa;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('mahasiswa', MahasiswaController::class);
